c44a0f6513537f8ac964f0b38de73dfb222969fb
Abdimalik-dev <cabdixuseen076@gmail.com>
2023-11-25 23:20:00 +0300

    commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function secondLargest(ak) {
-   ak.sort(function(a, b) {
+function secondLargest(data) {
+   data.sort(function(a, b) {
      return b - a;
    });
-   return ak[1];
+   return data[1];
  }
  console.log(secondLargest([44, 55, 66, 79, 88])); 
  console.log(secondLargest([10, 30, 86, 60, 90]));  
